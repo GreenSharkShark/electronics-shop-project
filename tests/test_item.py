9406de748481ef62f7531a2c60e38f4a52b27973
Zhorik Zeniuk <ZhorikZeniuk@gmail.ru>
2023-04-18 16:49:41 +0200

    create tests for item.py. Cov 100%

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+Item.pay_rate = 0.8
+item2.apply_discount()
+
+
+def test_item():
+    assert item1.calculate_total_price() == 200000
+    assert item2.price == 16000.0
